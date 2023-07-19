8ee5988951d3c01b5c3bf578e06f9f904cde4388
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2023-07-19 11:17:24 +0100

    Add jinja filters

---
+++ sohonet_nsot_helpers/jinja_filters.py
@@ -0,0 +1,97 @@
+# Sohonet custom Jinja2 Filters
+from passlib.hash import cisco_type7
+import base64
+from netutils.vlan import vlanlist_to_config
+import requests
+import ipaddress
+import re
+
+
+def encrypt_cisco_type7(password):
+    return cisco_type7.hash(password, salt=1)
+
+
+def encrypt_netiron_snmp(community):
+
+    # yapf: disable
+    table = {
+      'a': '!','b': '2','c': 'd','d': '@','e': 'n','f': 'G','g': '"','h': 'b','i': '=','j': '?','k': 'D','l': '^','m': '6','n': 'g','o': 's','p': 'S',
+      'q': 'R','r': 'U','s': '-','t': 'o','u': 'i','v': 'r','w': '+','x': 'C','y': '\\','z': 'x','A': 'q','B': 'L','C': 'B','D': ':','E': 'f','F': 'w',
+      'G': '9','H': '0','I': """'""",'J': 'c','K': 'h','L': '#','M': 'k','N': 't','O': ',','P': '5','Q': '_','R': 'P','S': '%','T': 'l','U': 'K','V': ']',
+      'W': 'a','X': 'E','Y': '*','Z': '(','0': 'Q','1': 'Z','2': '|','3': '8','4': '3','5': '0','6': 'm','7': 'Y','8': 'W','9': '{','!': 'V','"': 'F',
+      '#': 'I','$': '4','%': 'u','&': '>',"""'""": 'N','(': 'p',')': 'z','*': 'X','+': 'e',',': 'T','-': 'M','.': '&','/': ')',':': 'y',';': ';','<': '`',
+      '=': '$','>': 'v','@': '1','[': '7',']': '~','\\': 'H','^': '<','_': '}','`': '/','{': '.','}': 'j','~': '[',
+    }
+    # yapf: enable
+
+    cipher_string = ''
+    for char in community:
+        cipher_string += table[char]
+
+    base64_bytes = base64.b64encode(cipher_string.encode('ascii'))
+    return '$' + base64_bytes.decode('ascii')
+
+
+def mrv_physical_interfaces_to_config(ports):
+    ''' convert list of dicts containing interface names to a config line, filtering for numbered ports only
+
+        i.e. 1-16  or  1-13,15-16
+    '''
+    portlist = []
+    trunklist = []
+    for port in ports:
+        if port['type'] != 'VIRTUAL' and port['name'].isdigit():
+            if port['lag']:
+                if port['lag']['name'] not in trunklist:
+                    trunklist.append(port['lag']['name'])
+            else:
+                portlist.append(int(port['name']))
+
+    portlist_config = vlanlist_to_config(portlist, min_grouping_size=2)
+    if trunklist:
+        portlist_config[0] = portlist_config[0] + "," + ",".join(sorted(trunklist))
+    return portlist_config[0]
+
+
+def is_smn_ip(ipaddr):
+    ''' check if an ipaddress is in SMN ranges '''
+    req = requests.get(
+        'https://lon-proxy-03.storagesvc.sohonet.com/v1/AUTH_bc8242fea43146a7b8cee34a40f328e0/ip-ranges-PUBLIC-READABLE/smn-ip-ranges.json'
+    )
+    ip_ranges = req.json()
+
+    ip_to_check = ipaddress.ip_address(ipaddr.split('/')[0])
+
+    for prefix in ip_ranges['prefixes']:
+        if ip_to_check in ipaddress.ip_network(prefix['ip_prefix']):
+            return True
+
+    return False
+
+
+def netiron_normalized_interface_to_config(interface_name):
+    ''' Convert Netiron normalized interface names to names used in config
+
+    NORMALIZED                      CONFIG
+
+    GigabitEthernet1        ->      ethernet 1
+    GigabitEthernet1/24     ->      ethernet 1/24
+    10GigabitEthernet2/22   ->      ethernet 2/22
+    Ve1732                  ->      ve 1732
+    Loopback1               ->      loopback 1
+    Ethernetmgmt1           ->      management 1
+
+    '''
+
+    match = re.match(r'\d*(\D+)(\d+[\d|/]*)', interface_name)
+    if match:
+        if 'mgmt' in match.group(1):
+            return f"management {match.group(2)}"
+        elif 'Ethernet' in match.group(1):
+            return f"ethernet {match.group(2)}"
+        elif 'Ve' in match.group(1):
+            return f"ve {match.group(2)}"
+        elif 'Loopback' in match.group(1):
+            return f"loopback {match.group(2)}"
+
+    return interface_name
